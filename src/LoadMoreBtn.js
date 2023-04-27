9e607213d19439a9be4a943f511b1c8df018ccf6
yuliasiromakha <93142108+yuliasiromakha@users.noreply.github.com>
2023-04-27 21:53:52 +0300

    first commit

---
+++ src/LoadMoreBtn.js
@@ -0,0 +1,31 @@
+export default class LoadMoreBtn  {
+    constructor({selector, isHidden = false}) {
+        this.button = this.getButton(selector);
+
+        if (isHidden) {
+            this.hide();
+        }
+    }
+    
+    getButton(selector) {
+        return document.querySelector(selector);
+    }
+
+    disable() {
+        this.button.disabled = true;
+        this.button.textContent = 'Loading...'
+    }
+
+    enable() {
+        this.button.disabled = false;
+        this.button.textContent = 'Load more'
+    }
+    
+    hide() {
+        this.button.classList.add('hidden');
+    }
+
+    show() {
+        this.button.classList.remove('hidden');
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,51 +1,124 @@
 import Notiflix from 'notiflix';
-import { fetchingImages } from './api';
+import { CardApiService } from './api';
+import LoadMoreBtn from './loadMoreBtn';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-// const form = document.querySelector('.search-form')
 const form = document.getElementById('search-form')
 const searchButton = document.querySelector('.search-button');
 const input = document.querySelector('.search-input')
 const gallery = document.querySelector('.gallery')
 
+const cardApiService = new CardApiService();
+const loadMoreBtn = new LoadMoreBtn ({
+  selector: '#loadMore',
+  isHidden: true,
+});
+// console.log(loadMoreBtn);
+
 form.addEventListener('submit', onSubmit)
+
+loadMoreBtn.button.addEventListener('click', loadMoreCards)
 
 function onSubmit(e) {
     e.preventDefault()
 
+    loadMoreBtn.show()
     const inputValue = input.value.trim();
-    console.log(inputValue);
+    // console.log(inputValue);
+    if (!inputValue) {
+      return;
+    }
 
-    if (!inputValue) { 
-        return; 
-      }
+    cardApiService.query = inputValue;
 
-    fetchingImages(inputValue).then(data => {
-        const returnedResult = data.hits
-        console.log(returnedResult);
+    cardApiService.resetPage( )
+    clearSearchResult()
+    fetchCards().finally(() => form.reset())
+    // console.log(cardApiService);
 
-        const totalHits = data.totalHits;
-        if (returnedResult.length === 0) {
-            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-        } else if(returnedResult.length >= 1) {
-            const createdMarkup = returnedResult.reduce((acc, card) => acc + renderPictureCard(card), '');
-        gallery.insertAdjacentHTML('beforeend', createdMarkup);
-        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
-        }
-    })
-    .catch(error => {
-        if (error.message === '404') {
-            Notiflix.Notify.failure("Oops, no data was found!");
-        }
-        console.log(error);
-    });
+    //   cardApiService.fetchingImages(inputValue).then(data => {
+    //     const returnedResult = data.hits
+    //     console.log(returnedResult);
+
+    //     const totalHits = data.totalHits;
+    //     if (returnedResult.length === 0) {
+    //         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    //     } else if(returnedResult.length >= 1) {
+    //         const createdMarkup = returnedResult.reduce((acc, card) => acc + renderPictureCard(card), '');
+    //     gallery.insertAdjacentHTML('beforeend', createdMarkup);
+    //     Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
+    //     }
+    // })
+
+
+
+    // .catch(error => {
+    //     if (error.message === '404') {
+    //         Notiflix.Notify.failure("Oops, no data was found!");
+    //     }
+    //     console.log(error);
+    // });
 
  }
 
-//  function renderPicture({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
-    function renderPictureCard({ webformatURL, tags, likes, views, comments, downloads }) {
+//  ФУНКЦІЯ ЩО ФЕТЧИТЬ НА ДІСПЛЕЙ КАРТКИ ТА ПЕРЕВІРЯЄ ЧИ ПРИСУТНЯ ІНФОРМАЦІЯ
+ function fetchCards() {
+
+  return getCardMarkup()
+  .catch(error => {
+  if (error.message === '404') {
+      Notiflix.Notify.failure("Oops, no data was found!");
+  }
+  console.log(error);})
+}
+
+// ФУНКЦІЯ ЩО ПЕРЕВІРЯЄ НАЯВНІСТЬ РЕЗУЛЬТАТУ ТА РЕДЮСИТЬ КАРТКИ
+ function getCardMarkup() {
+  return cardApiService.fetchingImages(cardApiService.query).then(data => {
+    const returnedResult = data.hits
+    console.log(returnedResult);
+
+    const totalHits = data.totalHits;
+    console.log('total hits:', totalHits);
+    if (returnedResult.length === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    } else if(returnedResult.length >= 1) {
+        const createdMarkup = returnedResult.reduce((acc, card) => acc + renderPictureCard(card), '');
+    gallery.insertAdjacentHTML('beforeend', createdMarkup);
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
+    // return createdMarkup;
+    }
+})
+ }
+
+ function loadMoreCards() {
+
+  loadMoreBtn.isHidden = true;
+  return cardApiService.fetchingImages(cardApiService.query).then(data => {
+    const returnedResult = data.hits
+    console.log(returnedResult);
+
+    if (returnedResult.length === 0) {
+        Notiflix.Notify.failure('We are sorry, but you have reached the end of search results.')
+    } else if(returnedResult.length >= 1) {
+        const createdMarkup = returnedResult.reduce((acc, card) => acc + renderPictureCard(card), '');
+    gallery.insertAdjacentHTML('beforeend', createdMarkup);
+
+    }
+})
+ }
+
+ function clearSearchResult() {
+  gallery.innerHTML = '';
+ }
+
+ function renderPictureCard({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
     return `
     <div class="photo-card">
-  <img width=250 height=150 src=${webformatURL} alt="${tags}" loading="lazy" />
+    <img width=250 height=150 src=${webformatURL} alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b>Likes: ${likes}</b>
@@ -63,3 +136,25 @@ function onSubmit(e) {
 </div>
     `
  }
+
+//  return `
+//  <div class="photo-card">
+// <a href="${largeImageURL}>
+//  <img width=250 height=150 src=${webformatURL} alt="${tags}" loading="lazy" />
+// </a>
+// <div class="info">
+//  <p class="info-item">
+//    <b>Likes: ${likes}</b>
+//  </p>
+//  <p class="info-item">
+//    <b>Views: ${views}</b>
+//  </p>
+//  <p class="info-item">
+//    <b>Comments: ${comments}</b>
+//  </p>
+//  <p class="info-item">
+//    <b>Downloads: ${downloads}</b>
+//  </p>
+// </div>
+// </div>
+//  `
