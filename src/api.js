4048a6a78ff15a55ed5df5d5b907bf03acaad64b
yuliasiromakha <93142108+yuliasiromakha@users.noreply.github.com>
2023-04-29 17:34:24 +0300

    ready for handing in

--- src/api.js
+++ src/api.js
@@ -1,58 +1,24 @@
-// export function fetchingImages(name) {
-//     const BASE_URL = 'https://pixabay.com/api/'
-//     const API_KEY = '35566788-2396923f3520db2f530781152';
-    
-//     return fetch(`${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`)
-//     .then(data => {
-
-//         if (!data.ok) {
-//             throw new Error(data.status)
-//           }
-        
-//     return data.json()})
-
-// }
-
 export class CardApiService {
     static BASE_URL = 'https://pixabay.com/api/';
     static API_KEY = '35566788-2396923f3520db2f530781152';
     constructor() {
-        this.query = ''; 
-        this.page = 1;
-    } 
-
+      this.query = '';
+      this.page = 1;
+      this.per_page = 40;
+    }
+  
     fetchingImages() {
-
-        const url = `${CardApiService.BASE_URL}?key=${CardApiService.API_KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
-        return fetch(url)
-        .then(data => {
-    
-            if (!data.ok) {
-                throw new Error(data.status)
-              }
-        this.incrementPage()
-        return data.json()
-    })
-
+      const url = `${CardApiService.BASE_URL}?key=${CardApiService.API_KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.per_page}&page=${this.page}`;
+      return fetch(url).then(data => {
+        if (!data.ok) {
+          throw new Error(data.status);
+        }
+  
+        return data.json();
+      });
     }
-
+  
     resetPage() {
-        this.page = 1;
+      this.page = 1;
     }
-
-    incrementPage() {
-        this.page += 1 ;
-
-    }
-
-    // checkForLastPage() {
-    //     const currentPage = this.page;
-    //     const lastPage = this.page / 40;
-    // }
-}
-//  per-page
-//  response totalHits
-//  lastPage = totalHits/per-page
-//  mustSail? mustFlow
-//  does lastpage = currentpage
-//  notiflix + hide
+  }
--- src/index.js
+++ src/index.js
@@ -1,134 +1,241 @@
+// import Notiflix from 'notiflix';
+// import { CardApiService } from './api';
+// import LoadMoreBtn from './LoadMoreBtn';
+// const form = document.getElementById('search-form');
+// const searchButton = document.querySelector('.search-button');
+// const input = document.querySelector('.search-input');
+// const gallery = document.querySelector('.gallery');
+// const cardApiService = new CardApiService();
+// const loadMoreBtn = new LoadMoreBtn({
+//   selector: '#loadMore',
+//   isHidden: true,
+// });
+// form.addEventListener('submit', onSubmit);
+// loadMoreBtn.button.addEventListener('click', loadMoreCards);
+// loadMoreBtn.hide();
+// // ФУНКЦІЯ ПІСЛЯ САБМІТУ ФОРМИ
+// function onSubmit(e) {
+//   e.preventDefault();
+//   loadMoreBtn.show();
+//   const inputValue = input.value.trim();
+//   if (!inputValue) {
+//     loadMoreBtn.hide();
+//     return;
+//   }
+//   cardApiService.query = inputValue;
+//   cardApiService.resetPage();
+//   clearSearchResult();
+//   fetchCards().finally(() => form.reset());
+// }
+// //  ФУНКЦІЯ ЩО ФЕТЧИТЬ НА ДІСПЛЕЙ КАРТКИ ТА ПЕРЕВІРЯЄ ЧИ ПРИСУТНЯ ІНФОРМАЦІЯ
+// function fetchCards() {
+//   loadMoreBtn.hide();
+//   return getCardMarkup().catch(error => {
+//     if (error.message === '404') {
+//       Notiflix.Notify.failure('Oops, no data was found!');
+//     }
+//     console.log(error);
+//   });
+// }
+// // ФУНКЦІЯ ЩО ПЕРЕВІРЯЄ НАЯВНІСТЬ РЕЗУЛЬТАТУ ТА РЕДЮСИТЬ КАРТКИ
+// function getCardMarkup() {
+//   return cardApiService.fetchingImages(cardApiService.query).then(data => {
+//     const returnedResult = data.hits;
+//     console.log(returnedResult);
+//     const totalHits = data.totalHits;
+//     console.log('total hits:', totalHits);
+//     if (returnedResult.length === 0) {
+//       loadMoreBtn.hide();
+//       Notiflix.Notify.failure(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//     } else if (returnedResult.length >= 1) {
+//       const createdMarkup = returnedResult.reduce(
+//         (acc, card) => acc + renderPictureCard(card),
+//         ''
+//       );
+//       gallery.insertAdjacentHTML('beforeend', createdMarkup);
+//       Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+//       loadMoreBtn.show();
+//     }
+//     if (returnedResult.length > 0 && returnedResult.length < 40) {
+//       loadMoreBtn.hide();
+//     }
+//   });
+// }
+// function loadMoreCards() {
+//   cardApiService.page += 1;
+//   return cardApiService.fetchingImages(cardApiService.query).then(data => {
+//     const returnedResult = data.hits;
+//     const lastPage = Math.ceil(data.totalHits / cardApiService.per_page);
+//     if (returnedResult.length >= 1) {
+//       const createdMarkup = returnedResult.reduce(
+//         (acc, card) => acc + renderPictureCard(card),
+//         ''
+//       );
+//       gallery.insertAdjacentHTML('beforeend', createdMarkup);
+//     }
+//     if (lastPage === cardApiService.page) {
+//       Notiflix.Notify.info(
+//         'We are sorry, but you have reached the end of search results.'
+//       );
+//       loadMoreBtn.hide();
+//     }
+//   });
+// }
+// function clearSearchResult() {
+//   gallery.innerHTML = '';
+// }
+// function renderPictureCard({
+//   webformatURL,
+//   largeImageURL,
+//   tags,
+//   likes,
+//   views,
+//   comments,
+//   downloads,
+// }) {
+//   return `
+//     <div class='photo-card'>
+//     <img width=250 height=150 src=${webformatURL} alt='${tags}' loading='lazy' />
+//   <div class='info'>
+//     <p class='info-item'>
+//       <b>Likes: ${likes}</b>
+//     </p>
+//     <p class='info-item'>
+//       <b>Views: ${views}</b>
+//     </p>
+//     <p class='info-item'>
+//       <b>Comments: ${comments}</b>
+//     </p>
+//     <p class='info-item'>
+//       <b>Downloads: ${downloads}</b>
+//     </p>
+//   </div>
+// </div>
+//     `;
+// }
+
 import Notiflix from 'notiflix';
 import { CardApiService } from './api';
 import LoadMoreBtn from './LoadMoreBtn';
 
-const form = document.getElementById('search-form')
+const form = document.getElementById('search-form');
 const searchButton = document.querySelector('.search-button');
-const input = document.querySelector('.search-input')
-const gallery = document.querySelector('.gallery')
-
+const input = document.querySelector('.search-input');
+const gallery = document.querySelector('.gallery');
 const cardApiService = new CardApiService();
-const loadMoreBtn = new LoadMoreBtn ({
+const loadMoreBtn = new LoadMoreBtn({
   selector: '#loadMore',
   isHidden: true,
 });
-// console.log(loadMoreBtn);
 
-form.addEventListener('submit', onSubmit)
+form.addEventListener('submit', onSubmit);
+loadMoreBtn.button.addEventListener('click', loadMoreCards);
+loadMoreBtn.hide();
 
-loadMoreBtn.button.addEventListener('click', loadMoreCards)
-loadMoreBtn.hide()
-
-// ФУНКЦІЯ ПІСЛЯ САБМІТУ ФОРМИ
-
-function onSubmit(e) {
-    e.preventDefault()
-
-    loadMoreBtn.show()
-    const inputValue = input.value.trim();
-    if (!inputValue) {
-      return;
-    }
-
-    cardApiService.query = inputValue;
-
-    cardApiService.resetPage( )
-    clearSearchResult()
-    fetchCards().finally(() => form.reset())
-
- }
-
-//  ФУНКЦІЯ ЩО ФЕТЧИТЬ НА ДІСПЛЕЙ КАРТКИ ТА ПЕРЕВІРЯЄ ЧИ ПРИСУТНЯ ІНФОРМАЦІЯ
- function fetchCards() {
-
-  loadMoreBtn.hide()
-  return getCardMarkup()
-  .catch(error => {
-  if (error.message === '404') {
-      Notiflix.Notify.failure("Oops, no data was found!");
+async function onSubmit(e) {
+  e.preventDefault();
+  loadMoreBtn.show();
+  const inputValue = input.value.trim();
+  if (!inputValue) {
+    loadMoreBtn.hide();
+    return;
   }
-  console.log(error);})
+  cardApiService.query = inputValue;
+  cardApiService.resetPage();
+  clearSearchResult();
+  try {
+    await fetchCards();
+  } finally {
+    form.reset();
+  }
 }
 
-// ФУНКЦІЯ ЩО ПЕРЕВІРЯЄ НАЯВНІСТЬ РЕЗУЛЬТАТУ ТА РЕДЮСИТЬ КАРТКИ
- function getCardMarkup() {
-  return cardApiService.fetchingImages(cardApiService.query).then(data => {
-    const returnedResult = data.hits
-    console.log(returnedResult);
-
+async function fetchCards() {
+  loadMoreBtn.hide();
+  try {
+    const data = await cardApiService.fetchingImages(cardApiService.query);
+    const returnedResult = data.hits;
     const totalHits = data.totalHits;
     console.log('total hits:', totalHits);
-
-    // ПЕРЕВІРКА НА ОСТАННЮ СТОРІНКУ
-    const lastPage = totalHits / 40;
-    const currentPage = '...';
-
-    if (lastPage === currentPage) {
-      loadMoreBtn.hide()
-        Notiflix.Notify.failure('We are sorry, but you have reached the end of search results.')
-    }
-
-//  per-page
-//  response totalHits
-//  lastPage = totalHits/per-page
-//  mustSail? mustFlow
-//  does lastpage = currentpage
-//  notiflix + hide
-
     if (returnedResult.length === 0) {
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-    } else if(returnedResult.length >= 1) {
-        const createdMarkup = returnedResult.reduce((acc, card) => acc + renderPictureCard(card), '');
-    gallery.insertAdjacentHTML('beforeend', createdMarkup);
-    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
-    loadMoreBtn.show()
-    } 
+      loadMoreBtn.hide();
+      Notiflix.Notify.failure('Oops, no data was found!');
+    } else if (returnedResult.length >= 1) {
+      const createdMarkup = returnedResult.reduce(
+        (acc, card) => acc + renderPictureCard(card),
+        ''
+      );
+      gallery.insertAdjacentHTML('beforeend', createdMarkup);
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+      loadMoreBtn.show();
+    }
     if (returnedResult.length > 0 && returnedResult.length < 40) {
-      loadMoreBtn.hide()
+      loadMoreBtn.hide();
     }
-
-})
- }
-
- function loadMoreCards() {
-
-  return cardApiService.fetchingImages(cardApiService.query).then(data => {
-    const returnedResult = data.hits
-    console.log(returnedResult);
-
-    if (returnedResult.length === 0) {
-      loadMoreBtn.hide()
-        Notiflix.Notify.failure('We are sorry, but you have reached the end of search results.')
-
-    } else if(returnedResult.length >= 1) {
-        const createdMarkup = returnedResult.reduce((acc, card) => acc + renderPictureCard(card), '');
-    gallery.insertAdjacentHTML('beforeend', createdMarkup);
+  } catch (error) {
+    if (error.message === '404') {
+      Notiflix.Notify.failure('Oops, no data was found!');
     }
-})
- }
+    console.log(error);
+  }
+}
 
- function clearSearchResult() {
+async function loadMoreCards() {
+  cardApiService.page += 1;
+  try {
+    const data = await cardApiService.fetchingImages(cardApiService.query);
+    const returnedResult = data.hits;
+    const lastPage = Math.ceil(data.totalHits / cardApiService.per_page);
+    if (returnedResult.length >= 1) {
+      const createdMarkup = returnedResult.reduce(
+        (acc, card) => acc + renderPictureCard(card),
+        ''
+      );
+      gallery.insertAdjacentHTML('beforeend', createdMarkup);
+    }
+    if (lastPage === cardApiService.page) {
+      Notiflix.Notify.info(
+        'We are sorry, but you have reached the end of search results.'
+      );
+      loadMoreBtn.hide();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function clearSearchResult() {
   gallery.innerHTML = '';
- }
+}
 
- function renderPictureCard({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
-    return `
-    <div class="photo-card">
-    <img width=250 height=150 src=${webformatURL} alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
+function renderPictureCard({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `
+    <div class='photo-card'>
+    <img width=250 height=150 src=${webformatURL} alt='${tags}' loading='lazy' />
+  <div class='info'>
+    <p class='info-item'>
       <b>Likes: ${likes}</b>
     </p>
-    <p class="info-item">
+    <p class='info-item'>
       <b>Views: ${views}</b>
     </p>
-    <p class="info-item">
+    <p class='info-item'>
       <b>Comments: ${comments}</b>
     </p>
-    <p class="info-item">
+    <p class='info-item'>
       <b>Downloads: ${downloads}</b>
     </p>
   </div>
 </div>
-    `
- }
+    `;
+}
